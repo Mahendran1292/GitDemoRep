efa6119e3cd1fe4208c02a726dd754e3c71e5499
Mahendran1292 <mahendranc1292@gmail.com>
2024-08-15 19:51:46 +0530

    DEMOREP MESSAGE

---
+++ src/org/gitdemorep/GitDemoRep.java
@@ -0,0 +1,13 @@
+package org.gitdemorep;
+
+public class GitDemoRep {
+	private void today() {
+		System.out.println("Independence Day");
+
+	}
+	public static void main(String[] args) {
+		GitDemoRep g = new GitDemoRep();
+		g.today();
+	}
+
+}
